0d2811139c7ec4f47b5b076a95301cb722f1770b
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-07-22 23:29:49 -0300

    Documentado ussuario

--- include/usuario.h
+++ include/usuario.h
@@ -4,18 +4,27 @@
 #include <string>
 #include <iostream>
 
+// Classe para o modelo de usuarios do concordo
 class Usuario{
 	private:
-		std::string senha;
+		std::string senha;  //<! uma string com a senha do usuario
 
 	public:
-		int id;
-		std::string nome;
-		std::string email;
-		Usuario(int id, std::string nome, std::string email, std::string senha);
+		int id; //<! um inteiro com id do usuario
+		std::string nome; //<! uma string com nome do usuario
+		std::string email; //<! uma string com o email do usuario
+		Usuario(int id, std::string nome, std::string email, std::string senha);  //<! Costrutor incializando id, nome, email e a senha
 
+		/*! Get para o atributo email
+				@return retorna uma string com o email do usuario
+		*/
 		std::string get_email();
 
+		/*! Método para autenticar se o email e a senha do usuário estão corretas
+				@param email string com o suposto email do usuario
+				@param senha string com a suposta senha do usuario
+				@return um booleano contedo true caso o email e a senha do usuario estejam corretas e false caso o contrario
+		*/
 		bool auth(std::string email, std::string senha);
 };
 
