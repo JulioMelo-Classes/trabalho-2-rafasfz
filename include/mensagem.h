15e38740edb19203ddb21a940d93d9e12374da56
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-07-22 23:36:23 -0300

    Documentado mensagem.h

--- include/mensagem.h
+++ include/mensagem.h
@@ -6,14 +6,24 @@
 #include <vector>
 #include <usuario.h>
 
+// Classe para o modelo de mensagens do concordo
 class Mensagem{
 	private:
-		std::string dataHora;
-		int enviadaPor;
-		std::string conteudo;
+		std::string dataHora; //<! string com a data e hora de quando a mensagem foi enviada
+		int enviadaPor; //<! inteiro com o id do usuario que enviou a mensagem
+		std::string conteudo;  //<! string com o conteudo da mensagem
 
 	public:
+		/*! Construtor para incializar a classe
+				@param enviadaPor inteiro com o id do usuario que enviou a mensagem
+				@param conteudo string contendo o conteudo da 
+		*/
 		Mensagem(int enviadaPor, std::string conteudo);
+
+		/*! Recupera a mensagem com a string formatada
+				@param usuarios vetor de usuarios para recuperar o nome de o usuario pelo id
+				@return string com a mensagem formatada
+		*/
 		std::string get_message(std::vector<Usuario> usuarios);
 
 };
